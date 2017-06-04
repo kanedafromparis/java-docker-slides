37e991c05f2d7a391247cb93e8a19465eeb5a2ec
Charles Sabourdin <kanedafromparis@gmail.com>
2017-06-04 13:07:56 +0200

    scenarii in progress

--- src/main/dockerfiles/bash/coyote.sh
+++ src/main/dockerfiles/bash/coyote.sh
@@ -8,14 +8,18 @@ JVM_OPTIONS=" ";
 
 case $COYOTE_STAGE in
  	0) JVM_OPTIONS=" " ;;
-	1) JVM_OPTIONS=" -XshowSettings:vm -XX:+PrintGCDetails " ;;
-  2) JVM_OPTIONS=" -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/dump -XshowSettings:vm -XX:+PrintGCDetails" ;;
-	3) JVM_OPTIONS=" -Xms256M -Xmx512M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/dump -XshowSettings:vm -XX:+PrintGCDetails" ;;
-	4) JVM_OPTIONS=" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XshowSettings:vm -XX:+PrintGCDetails" ;;
-	5) JVM_OPTIONS=" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Xms256M -Xmx512M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/dump -XshowSettings:vm -XX:+PrintGCDetails" ;;
-	6) JVM_OPTIONS=" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/dump -XshowSettings:vm -XX:+PrintGCDetails" ;;
-	7) JVM_OPTIONS=" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=0.8 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/dump -XshowSettings:vm -XX:+PrintGCDetails" ;;
-  8) JVM_OPTIONS=" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=2 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/dump -XshowSettings:vm -XX:+PrintGCDetails" ;;
+ 	1) JVM_OPTIONS=" -Xms256M -Xmx512M " ;;
+ 	2) JVM_OPTIONS=" -Xms512M -Xmx2048M " ;;
+  
+	3) JVM_OPTIONS=" -XshowSettings:vm -XX:+PrintGCDetails " ;;
+  4) JVM_OPTIONS=" -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/dump -XshowSettings:vm -XX:+PrintGCDetails" ;;
+	5) JVM_OPTIONS=" -Xms256M -Xmx512M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/dump -XshowSettings:vm -XX:+PrintGCDetails" ;;
+	6) JVM_OPTIONS=" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XshowSettings:vm -XX:+PrintGCDetails" ;;
+	  
+	7) JVM_OPTIONS=" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Xms256M -Xmx512M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/dump -XshowSettings:vm -XX:+PrintGCDetails" ;;
+	8) JVM_OPTIONS=" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/dump -XshowSettings:vm -XX:+PrintGCDetails" ;;
+	9) JVM_OPTIONS=" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=0.8 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/dump -XshowSettings:vm -XX:+PrintGCDetails" ;;
+  10) JVM_OPTIONS=" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=2 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/dump -XshowSettings:vm -XX:+PrintGCDetails" ;;
 	*) echo "INVALID NUMBER!" ;;
 esac
 
